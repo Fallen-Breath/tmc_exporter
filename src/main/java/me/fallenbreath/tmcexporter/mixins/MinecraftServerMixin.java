da6e2f49a66a40fca8cd75cd5daa6bfa27e37418
Fallen_Breath <nellaforax@outlook.com>
2025-03-03 01:41:48 +0800

    tmc exporter init

--- src/main/java/me/fallenbreath/tmcexporter/mixins/MinecraftServerMixin.java
+++ src/main/java/me/fallenbreath/tmcexporter/mixins/MinecraftServerMixin.java
@@ -1,24 +1,24 @@
 /*
- * This file is part of the TemplateMod project, licensed under the
+ * This file is part of the TMC Exporter project, licensed under the
  * GNU Lesser General Public License v3.0
  *
- * Copyright (C) 2023  Fallen_Breath and contributors
+ * Copyright (C) 2025  Fallen_Breath and contributors
  *
- * TemplateMod is free software: you can redistribute it and/or modify
+ * TMC Exporter is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * TemplateMod is distributed in the hope that it will be useful,
+ * TMC Exporter is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with TemplateMod.  If not, see <https://www.gnu.org/licenses/>.
+ * along with TMC Exporter.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package me.fallenbreath.template_mod.mixins;
+package me.fallenbreath.tmcexporter.mixins;
 
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.MinecraftServer;
